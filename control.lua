18a34618acfca2e3876ae4d63e95f48edb405168
Noah Morton <noahmorton98@gmail.com>
2017-06-11 10:11:24 -0500

    Added user settings

--- control.lua
+++ control.lua
@@ -4,18 +4,6 @@ require("largeRuins")
 
 local DEBUG = false --used for debug, users should not enable
 
--- USER CONFIG VARS
--- These *must* match the server that you are playing on, and must not be changed during runtime, otherwise desyncs will occur.
-
---Minimum spawn distance - Change to give yourself more breathing room at spawn. Default 200.
-local MINIMUM_DISTANCE_FROM_SPAWN = 200
-local smallChance = 0.1
-local mediumChance = 0.05
-local largeChance = 0.02
-
-
---- BEGIN SCRIPTING
-
 --function that will return true 'percent' of the time.
 function probability(percent)
     return math.random() <= percent
@@ -24,9 +12,9 @@ end
 script.on_event({defines.events.on_chunk_generated},
     function (e)
         local center = {x=(e.area.left_top.x+e.area.right_bottom.x)/2, y=(e.area.left_top.y+e.area.right_bottom.y)/2}
-        if math.abs(center.x) < MINIMUM_DISTANCE_FROM_SPAWN and math.abs(center.y) < MINIMUM_DISTANCE_FROM_SPAWN then return end --too close to spawn
+        if math.abs(center.x) < settings.global["ruins-min-distance-from-spawn"].value and math.abs(center.y) < settings.global["ruins-min-distance-from-spawn"].value then return end --too close to spawn
 
-        if probability(smallChance) then
+        if probability(settings.global["ruins-small-ruin-chance"].value) then
             --spawn small ruin
             if DEBUG then
                 game.print("A small ruin was spawned at " .. center.x .. "," .. center.y)
@@ -38,7 +26,7 @@ script.on_event({defines.events.on_chunk_generated},
 
 
             --spawnSmallRuins(center)
-        elseif probability(mediumChance) then
+        elseif probability(settings.global["ruins-medium-ruin-chance"].value) then
             --spawn medium ruin
             if DEBUG then
                 game.print("A medium ruin was spawned at " .. center.x .. "," .. center.y)
@@ -50,7 +38,7 @@ script.on_event({defines.events.on_chunk_generated},
 
             spawnMediumRuins(center)
 
-        elseif probability(largeChance) then
+        elseif probability(settings.global["ruins-large-ruin-chance"].value) then
             --spawn large ruin
             if DEBUG then
                 game.print("A large ruin was spawned at " .. center.x .. "," .. center.y)
