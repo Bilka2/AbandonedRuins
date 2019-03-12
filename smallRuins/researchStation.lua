4216f3cc23afd1f63491032a8a10b7905b7f6379
Bilka <Bilka2@users.noreply.github.com>
2019-03-12 14:10:34 +0100

    Update to 0.17

--- smallRuins/researchStation.lua
+++ smallRuins/researchStation.lua
@@ -3,5 +3,5 @@ return function(center, surface) --research station
     local ce = surface.create_entity --save typing
     local fN = game.forces.neutral
     ce{name = "lab", position = {center.x + 1.5, center.y-0.5}, force = fN}
-    ce{name = "wooden-chest", position = {center.x-1.5, center.y + 0.5}, force = fN}.insert{name = "science-pack-1", count=20}
+    ce{name = "wooden-chest", position = {center.x-1.5, center.y + 0.5}, force = fN}.insert{name = "automation-science-pack", count=20}
 end
