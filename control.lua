850b0557f47691a5878c8dc7a8bd61ad9134622e
Bilka <Bilka2@users.noreply.github.com>
2020-09-16 18:27:23 +0200

    Moved center of chunk to utils, removed debug prints

--- control.lua
+++ control.lua
@@ -3,8 +3,6 @@ local small_ruins = require("smallRuins")
 local medium_ruins = require("mediumRuins")
 local large_ruins = require("largeRuins")
 
-local DEBUG = false -- used for debug, users should not enable
-
 local function spawn_chances()
     local smallChance = settings.global["ruins-small-ruin-chance"].value
     local mediumChance = settings.global["ruins-medium-ruin-chance"].value
@@ -26,38 +24,24 @@ script.on_event(defines.events.on_runtime_mod_setting_changed, spawn_chances)
 
 script.on_event(defines.events.on_chunk_generated,
     function (e)
-        local center = {x=(e.area.left_top.x+e.area.right_bottom.x)/2, y=(e.area.left_top.y+e.area.right_bottom.y)/2}
+        local center = util.get_center_of_chunk(e.position)
         if math.abs(center.x) < settings.global["ruins-min-distance-from-spawn"].value and math.abs(center.y) < settings.global["ruins-min-distance-from-spawn"].value then return end --too close to spawn
 
         local spawn_type = math.random()
-        if spawn_type <= global.spawn_table.small then
-            --spawn small ruin
-            if DEBUG then
-                game.print("A small ruin was spawned at " .. center.x .. "," .. center.y)
-            end
-
+        if spawn_type <= global.spawn_table.small then --spawn small ruin
             --random variance so they aren't always chunk aligned
             center.x = center.x + math.random(-10,10)
             center.y = center.y + math.random(-10,10)
 
-            util.spawn_ruin(small_ruins, util.SMALL_RUIN_HALF_SIZE, center, e.surface)
-        elseif spawn_type <= global.spawn_table.medium then
-            --spawn medium ruin
-            if DEBUG then
-                game.print("A medium ruin was spawned at " .. center.x .. "," .. center.y)
-            end
-
+            util.spawn_random_ruin(small_ruins, util.SMALL_RUIN_HALF_SIZE, center, e.surface)
+        elseif spawn_type <= global.spawn_table.medium then --spawn medium ruin
             --random variance so they aren't always chunk aligned
             center.x = center.x + math.random(-5,5)
             center.y = center.y + math.random(-5,5)
 
-            util.spawn_ruin(medium_ruins, util.MEDIUM_RUIN_HALF_SIZE, center, e.surface)
-        elseif spawn_type <= global.spawn_table.large then
-            --spawn large ruin
-            if DEBUG then
-                game.print("A large ruin was spawned at " .. center.x .. "," .. center.y)
-            end
-            util.spawn_ruin(large_ruins, util.LARGE_RUIN_HALF_SIZE, center, e.surface)
+            util.spawn_random_ruin(medium_ruins, util.MEDIUM_RUIN_HALF_SIZE, center, e.surface)
+        elseif spawn_type <= global.spawn_table.large then --spawn large ruin
+            util.spawn_random_ruin(large_ruins, util.LARGE_RUIN_HALF_SIZE, center, e.surface)
         end
     end
 )
--- utilities.lua
+++ utilities.lua
@@ -3,6 +3,10 @@ util.SMALL_RUIN_HALF_SIZE = 8 / 2
 util.MEDIUM_RUIN_HALF_SIZE = 16 / 2
 util.LARGE_RUIN_HALF_SIZE = 32 / 2
 
+util.get_center_of_chunk = function(chunk_position)
+    return {x = chunk_position.x * 32 + 16, y = chunk_position.y * 32 + 16}
+end
+
 local function clear_area(half_size, center, surface)
     local area = {{center.x-half_size, center.y-half_size}, {center.x+half_size, center.y+half_size}}
     --exclude tiles that we shouldn't spawn on
@@ -10,17 +14,22 @@ local function clear_area(half_size, center, surface)
         return false
     end
 
-    for index, entity in pairs(surface.find_entities_filtered({area = area, type={"resource", "tree"}, invert = true})) do
+    for _, entity in pairs(surface.find_entities_filtered({area = area, type={"resource", "tree"}, invert = true})) do
         entity.destroy({do_cliff_correction=true})
     end
 
     return true
 end
 
-util.spawn_ruin = function(ruins, half_size, center, surface)
+util.spawn_ruin = function(ruin, half_size, center, surface)
     if clear_area(half_size, center, surface) then
-        ruins[math.random(#ruins)](center, surface) --call a random function
+        ruin(center, surface)
     end
+end
+
+util.spawn_random_ruin = function(ruins, half_size, center, surface)
+    --spawn a random ruin from the list
+    util.spawn_ruin(ruins[math.random(#ruins)], half_size, center, surface)
 end
 
 return util
