b8ab6cf3e3e9cb6fb16789b89c8bc6a37e6bfedb
Bilka <Bilka2@users.noreply.github.com>
2020-09-18 20:41:02 +0200

    Removed research station ruins that did not contain labs

--- smallRuins/researchStation5.lua
+++ smallRuins/researchStation5.lua
@@ -6,8 +6,5 @@ return function(center, surface) --research station
     end
     local fN = game.forces.neutral
     ce{name = "lab", position = {center.x + 1.5, center.y-0.5}, force = fN}
-    local e = ce{name = "wooden-chest", position = {center.x-1.5, center.y + 0.5}, force = fN}
-    if e then
-      e.insert{name = "chemical-science-pack", count = math.random(25, 100)}
-    end
+    ce{name = "wooden-chest", position = {center.x-1.5, center.y + 0.5}, force = fN}
 end
