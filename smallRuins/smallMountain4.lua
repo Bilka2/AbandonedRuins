add9d9cce09534122b6c06ec787d3f27693d3d59
Bilka <Bilka2@users.noreply.github.com>
2020-09-18 21:56:44 +0200

    Moved two more entities into the ruin size

--- smallRuins/smallMountain4.lua
+++ smallRuins/smallMountain4.lua
@@ -5,7 +5,7 @@ return function(center, surface) --small mountain
     end
     local fN = game.forces.neutral
     ce{name = "rock-huge", position = {center.x + (-1.57), center.y + (-1.37)}}
-    ce{name = "rock-huge", position = {center.x + (2.83), center.y + (-2.73)}}
+    ce{name = "rock-huge", position = {center.x + (2.43), center.y + (-2.33)}}
     ce{name = "rock-huge", position = {center.x + (-2), center.y + (2)}}
     ce{name = "rock-huge", position = {center.x + (1.12), center.y + (1.97)}}
 end
