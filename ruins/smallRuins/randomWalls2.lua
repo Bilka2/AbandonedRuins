5db340be36844e38e16e27538c0eb050fcd7c2d1
Bilka <Bilka2@users.noreply.github.com>
2020-09-24 13:14:43 +0200

    Added 15 more ruins

--- ruins/smallRuins/randomWalls2.lua
+++ ruins/smallRuins/randomWalls2.lua
@@ -2,12 +2,16 @@ return
 {
   entities =
   {
-    {"stone-wall", {x = -2.5, y = -2.5}, {}},
-    {"stone-wall", {x = 0.5, y = -2.5}, {}},
-    {"stone-wall", {x = -1.5, y = -0.5}, {dmg = {dmg = {type = "random", min = 1, max = 75}}, }},
-    {"stone-wall", {x = 2.5, y = -0.5}, {}},
-    {"stone-wall", {x = -2.5, y = 3.5}, {}},
-    {"stone-wall", {x = -1.5, y = 3.5}, {}},
-    {"stone-wall", {x = -0.5, y = 2.5}, {}},
+    {"stone-wall", {x = -2.5, y = -2.5}, {dead = 0.3}},
+    {"stone-wall", {x = -1.5, y = -2.5}, {dead = 0.3}},
+    {"stone-wall", {x = 1.5, y = -2.5}, {dead = 0.3}},
+    {"stone-wall", {x = 2.5, y = -2.5}, {dead = 0.3}},
+    {"stone-wall", {x = -2.5, y = -1.5}, {dead = 0.3}},
+    {"stone-wall", {x = 2.5, y = -1.5}, {dead = 0.3}},
+    {"stone-wall", {x = -2.5, y = 0.5}, {dead = 0.3}},
+    {"wooden-chest", {x = 0.5, y = 0.5}, {dead = 0.3}},
+    {"stone-wall", {x = -0.5, y = 2.5}, {dead = 0.3}},
+    {"stone-wall", {x = -1.5, y = 2.5}, {dead = 0.3}},
+    {"stone-wall", {x = 1.5, y = 2.5}, {dead = 0.3}},
   },
 }
--- ruins/smallRuins/splitterI3.lua
+++ ruins/smallRuins/splitterI3.lua
@@ -2,6 +2,6 @@ return
 {
   entities =
   {
-    {"fast-splitter", {x = 1, y = -0.5}, {dir = "south", }},
+    {{type = "random-of-entity-type", entity_type = "splitter"}, {x = 1, y = -0.5}, {dir = "south", dmg = {dmg = {type="random", min = 0, max = 50}}}},
   },
 }
