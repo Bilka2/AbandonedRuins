42bfbfb5b67be3cdeed76bb6241b42164319abdf
Bilka <Bilka2@users.noreply.github.com>
2022-01-01 12:43:54 +0100

    Fixed that oil pumpjack ruins would spawn oil patches
    Fixes #22

--- ruins/smallRuins/oilPumpjack.lua
+++ ruins/smallRuins/oilPumpjack.lua
@@ -3,7 +3,6 @@ return
   entities =
   {
     {"pumpjack", {x = -0.5, y = -2.5}, {dir = "east", dead = 1}},
-    {"crude-oil", {x = -0.5, y = -2.5}, {}},
     {"storage-tank", {x = 2.5, y = -2.5}, {dmg = {dmg = {type = "random", min = 0, max = 80}}, dead = 0.4}},
     {"medium-electric-pole-remnants", {x = 0.5, y = -0.5}, {}},
     {"storage-tank", {x = 2.5, y = 0.5}, {dir = "east", dmg = {dmg = {type = "random", min = 0, max = 80}}, dead = 0.4}},
